35ce59a50ba7d0c5a9977408d236aeba3f8c85b1
PieroPazC <pieropaz13@gmail.com>
2018-09-04 13:02:08 -0500

    Avanzamos con Unity

---
+++ JorgePazCordova/Assets/Movimiento.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+    public Vector3 position;
+    public float speed = 1;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        position.x += speed * Time.deltaTime;
+        transform.position = position;
+    }
+}
