06af33e62ca933faa464e5b306a6c7218f9cb0a6
PieroPazC <pieropaz13@gmail.com>
2018-09-11 13:38:34 -0500

    Cubo en movimiento y laberinto

--- JorgePazCordova/Assets/MovimientoControlado.cs
+++ JorgePazCordova/Assets/MovimientoControlado.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 
 public class MovimientoControlado : MonoBehaviour {
 
-    public float speed = 6;
+    public float speed = 5;
+    public float angSpeed = 50;
+
+    Rigidbody controlRB;
 
 	// Use this for initialization
 	void Start () {
-
+        controlRB = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            transform.position += Vector3.up * Time.deltaTime * speed;
-        } else if (Input.GetKey(KeyCode.DownArrow)) {
-            transform.position += Vector3.down * Time.deltaTime * speed;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //transform.Translate (Vector3.forward * Time.deltaTime * speed);
+            controlRB.velocity = Vector3.forward * speed;
         }
-
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //transform.Translate (Vector3.back * Time.deltaTime * speed);
+            controlRB.velocity = Vector3.back * speed;
+        }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += Vector3.left * Time.deltaTime * speed;
+            //)).Translate (Vector3.left * Time.deltaTime * speed);
+            controlRB.velocity = Vector3.left * speed;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right * Time.deltaTime * speed;
+            //transform.Translate (Vector3.right * Time.deltaTime * speed);
+            controlRB.velocity = Vector3.right * speed;
         }
     }
+
 }
---
+++ JorgePazCordova/Assets/colision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void OnTriggerEnter (Collider other) {
+        Destroy(other.gameObject);
+	}
+}
