a007e66de87b121c4287bd2a0ea0a13019ef13e2
PieroPazC <pieropaz13@gmail.com>
2018-09-13 13:08:47 -0500

    Comenzamos 2D

---
+++ JorgePazCordova2D/Assets/MovimientoControlado2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoControlado2D : MonoBehaviour {
+
+    public float speed;
+    public int limit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A)&& transform.position.x > limit){
+            transform.Translate(Vector3.left);
+        } else if (Input.GetKeyDown(KeyCode.D)){
+            transform.Translate(Vector3.right);
+        } else if (Input.GetKeyDown(KeyCode.S)){
+            transform.Translate(Vector3.down);
+        } else if (Input.GetKeyDown(KeyCode.W)){
+            transform.Translate(Vector3.up);
+        }
+	}
+}
