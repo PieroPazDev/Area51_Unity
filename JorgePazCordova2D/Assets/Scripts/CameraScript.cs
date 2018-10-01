ff418b54cfff91e20805eeb467aef39f883dd5d4
PieroPazC <pieropaz13@gmail.com>
2018-10-01 18:43:14 -0500

    Seguimiento de Camara en el jugador

---
+++ JorgePazCordova2D/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    private Transform player;
+    public float topLimit;
+    public float downLimit;
+    public float leftLimit;
+    public float rigthLimit;
+
+    void Start()
+    {
+        player = GameObject.Find("Character").transform;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector2(Mathf.Clamp(player.position.x, leftLimit, rigthLimit),
+                                         Mathf.Clamp(player.position.y, downLimit, topLimit));
+                                        
+    }
+}
