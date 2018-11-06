77314606365e4f3d806bfd31a65fc32809490a10
PieroPazC <pieropaz13@gmail.com>
2018-11-06 13:02:44 -0500

    Empezando 3D
    Movimiento ,Rotacion y Salto de Personaje

---
+++ JorgePazCordova2D/Assets/2D Project/Scripts/ButtonActions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonActions : MonoBehaviour {
+
+    public void LoadScene (int index) {
+        SceneManager.LoadScene (index);
+    }
+
+    public void QuitGame () {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
---
+++ JorgePazCordova3D/JorgePazCordova3D/Assets/Scripts/CharacterBaseMove3D.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBaseMove3D : MonoBehaviour {
+
+    Rigidbody rigBod;
+    Vector3 movement;
+    Vector3 respawnPos;
+
+    public float speed = 2f;
+    public float jumpForce = 10f;
+    public float angSpeed = 125f;
+
+	void Start () {
+        rigBod = GetComponent<Rigidbody>();
+        respawnPos = transform.position;
+	}
+
+	private void Update()
+	{
+        if (Input.GetKeyDown(KeyCode.Space)){
+            rigBod.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        }
+	}
+
+	void FixedUpdate (){
+        movement = transform.forward * Input.GetAxis("Vertical");
+        rigBod.MovePosition(rigBod.position + (movement * speed * Time.fixedDeltaTime));
+        Quaternion rotation =  Quaternion.Euler(Vector3.up * Input.GetAxis("Horizontal") * angSpeed * Time.fixedDeltaTime);
+        rigBod.MoveRotation(rotation * rigBod.rotation);
+
+	}
+
+    void Respawn(){
+        rigBod.velocity = Vector3.zero;
+        rigBod.MovePosition(respawnPos);
+    }
+
+	private void OnTriggerExit(Collider other){
+        if (other.CompareTag("PlayArea")){
+            Respawn();
+        }
+	}
+	private void OnDrawGizmos()
+	{
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward);
+	}
+}
