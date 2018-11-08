0dc14e406f28b8a7821f9bbcdcf7a785f7e1418e
PieroPazC <pieropaz13@gmail.com>
2018-11-08 13:07:08 -0500

    Comenzando con struct

---
+++ JorgePazCordova3D/Assets/Scripts/CamBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamBehaviour : MonoBehaviour {
+
+    //[System.Serializable]
+    public struct TargetData {
+        public Transform target;
+        public Vector3 focusDistance;
+        public Vector3 followDistance;
+        public float minFollowDistance;
+
+        public void SetFromVariables(Transform target, Vector3 focus, Vector3 follow, float minSpeed){
+            this.target = target;
+            focusDistance = focus;
+            followDistance = follow;
+            minFollowDistance = minSpeed;
+        }
+    }
+
+    public Transform target;
+    public Vector3 focusDistance;
+    Vector3 focusPoint { get { return target.position + GetRelativePos(target, focusDistance); } }
+    public Vector3 followDistance;
+    Vector3 followPoint { get { return target.position + GetRelativePos(target, followDistance); } }
+    public float minFollowSpeed = 1f;
+    public float followDistanceDelta { get { return Vector3.Distance (transform.position, followDistance); }}
+
+    public TargetData defaultData;
+
+    void Start () {
+
+    }
+
+    void LateUpdate () {
+        transform.position = Vector3.MoveTowards(transform.position , followPoint, (minFollowSpeed + followDistanceDelta) * Time.deltaTime);
+        transform.LookAt (focusPoint);
+    }
+
+    Vector3 GetRelativePos (Transform relativeTarget, Vector3 distance){
+        Vector3 relativePos;
+        relativePos = relativeTarget.right * distance.x;
+        relativePos += relativeTarget.up * distance.y;
+        relativePos += relativeTarget.forward * distance.z;
+
+        return relativePos;
+    }
+
+    public void Reposition(Vector3 updatedPosition){
+        transform.position = updatedPosition;
+    }
+
+    private void OnDrawGizmos () {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine (followPoint, focusPoint);
+        Gizmos.DrawSphere (focusPoint, 0.25f);
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere (followPoint, 0.25f);
+    }
+}
---
+++ JorgePazCordova3D/Assets/Scripts/CharacterBaseMov3D.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBaseMov3D : MonoBehaviour {
+
+    public struct CharTransformData{
+        public Vector3 position;
+        public Quaternion rotation;
+    }
+
+    Rigidbody rigBod;
+    Vector3 movement;
+
+    CharTransformData respawnData;
+    Vector3 respawnPos;
+
+    public float speed = 5f;
+    public float angSpeed = 25f;
+    public float jumpForce = 10f;
+
+	// Use this for initialization
+	void Start () {
+        rigBod = GetComponent<Rigidbody>();
+        respawnData.position = transform.position;
+        respawnData.rotation = transform.rotation;
+	}
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rigBod.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        }
+	}
+
+	void FixedUpdate () {
+        movement = transform.forward * Input.GetAxis("Vertical");
+        rigBod.MovePosition(rigBod.position + (movement * speed * Time.fixedDeltaTime));
+        Quaternion rotation = Quaternion.Euler(Vector3.up * Input.GetAxis("Horizontal") * angSpeed * Time.fixedDeltaTime);
+        rigBod.MoveRotation(rotation * rigBod.rotation);
+	}
+
+    void Respawn () {
+        rigBod.velocity = Vector3.zero;
+        Reposition (respawnData);
+        rigBod.MovePosition(respawnData.position);
+        CamBehaviour playerCam = Camera.main.GetComponent<CamBehaviour> ();
+        playerCam.Reposition(respawnPos + playerCam.followDistance);
+    }
+
+    public void Reposition (CharTransformData transformData){
+        rigBod.MovePosition (respawnData.position);
+        rigBod.MoveRotation (respawnData.rotation);
+    }
+
+	void OnTriggerExit (Collider other) {
+        if (other.CompareTag("GameArea")) {
+            Respawn();
+        }
+	}
+	void OnDrawGizmos () {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward);
+	}
+}
